51a4483f8d89431cfbeb3299c2dbb92ff1c200f8
malvinnthapeleng <malvinnthapeleng>
2023-08-10 19:44:12 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Use the puts function to print the string */
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
