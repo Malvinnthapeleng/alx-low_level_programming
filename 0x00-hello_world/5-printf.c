17887d9e8930b31afedb7723692574bec903006f
malvinnthapeleng <malvinnthapeleng>
2023-08-10 20:10:22 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Use the printf function to print the string */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
