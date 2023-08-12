2fd53f79b32335964261b5211b48316db7a64ad1
malvinnthapeleng <malvinnthapeleng>
2023-08-12 16:30:01 -0700

    3-print_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,11 +4,12 @@
  * main -  function to print alphabets in lowercase and uppercase
  * Return: (0) (Success)
  */
-	
+
 int main(void)
 {
 	char lowercase = 'a';
 	char uppercase = 'A';
+
 	while (lowercase <= 'z')
 	{
 		putchar(lowercase);
