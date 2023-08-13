453523bc460d3691f7c8aedeb675474223465589
malvinnthapeleng <malvinnthapeleng>
2023-08-13 15:42:52 -0700

    101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,11 +11,11 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 7; i++)
 	{
-		for (j = i + 1; j < 10; j++)
+		for (j = i + 1; j <= 8; j++)
 		{
-			for (k = j + 1; k < 10; k++)
+			for (k = j + 1; k <= 9; k++)
 			{
 				putchar(i + '0');
 				putchar(j + '0');
