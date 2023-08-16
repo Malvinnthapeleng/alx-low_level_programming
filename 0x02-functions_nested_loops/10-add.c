cdfa7a924fe529e8a11965af09a699194bf4c616
malvinnthapeleng <malvinnthapeleng>
2023-08-16 14:58:31 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds to values
+ * @n1: input first value to be added
+ * @n2: input second value to be added
+ * Return: the sum of two integers
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+int add(int, int)}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * jack_bauer - function that prints every minute of the day of Jack Bauer
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar((hour / 10) + 48);
+			_putchar((hour % 10) + 48);
+			_putchar(':');
+			_putchar((minute / 10) + 48);
+			_putchar((minute % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
