276c209a2012f2206eda75c62f30110db163925f
malvinnthapeleng <malvinnthapeleng>
2023-08-16 03:28:38 -0700

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,16 +2,16 @@
 /**
 *print_sign - function that prints the sign of a number.
 *@n:input number as integer
-*Return: (1) if n greater than zero. (0) if n is zero. -1 if n is less than zero
+*Return: (1) if n greater than zero.(0). 0 if n is zero.else  -1
 */
 
 int print_sign(int n)
 {
 	if (n > 0)
- {
+	{
 	 _putchar(43);
 	 return (1);
- }
+	}
 	else if (n < 0)
 	{
 		_putchar(45);
