471b91955ce7eac124c461467b5966c1089608f6
malvinnthapeleng <malvinnthapeleng>
2023-08-18 11:20:50 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,19 +9,26 @@ void print_triangle(int size)
 int i;
 int j;
 
-if (size <= 0)
+if (size > 0)
 {
-_putchar('\n');
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (j < size - (i + 1))
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('#');
+			}
+		}
+		_putchar('\n');
+	}
 }
 else
 {
-for ( i = 1; i <= size; i++)
-{
-for (j = 0; j < i; j++)
-{
-_putchar('#');
+	_putchar('\n');
 }
-}
-}
-_putchar('\n');
 }
