51c26bc835d622d91c9c6120bdec322b383d513a
malvinnthapeleng <malvinnthapeleng>
2023-08-18 09:40:33 -0700

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i < 100; i++)
 	{
 		if ((i % 3) == 0 && ((i % 5) == 0))
 		{
@@ -28,6 +28,8 @@ int main(void)
 
 		}
 	}
+	printf("Buzz");
+	printf("\n");
 
 	return (0);
 }
