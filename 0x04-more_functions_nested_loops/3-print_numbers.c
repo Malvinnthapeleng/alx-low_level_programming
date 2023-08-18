992fa0114ee91580610add7b0ec53d81efe5004c
malvinnthapeleng <malvinnthapeleng>
2023-08-17 20:38:22 -0700

    _putchar

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,3 +1,17 @@
 #include "main.h"
 /**
- * mul- 
+ * print_numbers - function that prints the numbers, from 0 to 9
+ * Return: no return
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		_putchar(n);
+
+	}
+	_putchar('\n');
+}
