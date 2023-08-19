aef4a9c7b87b1ddcb35b0720a7f3a33b6be306e8
malvinnthapeleng <malvinnthapeleng>
2023-08-19 10:26:27 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	long int fib[50];
+
+	fib[0] = 1;
+	fib[2] = 2;
+	printf("%ld, %ld, ", fib[0], fib[1]);
+
+	for (i = 2; i < 50; i++)
+	{
+		fib[i] = fib[i - 1] + fib[i - 2];
+		if (i == 49)
+		{
+			printf("%ld\n", fib[i]);
+		}
+		else
+		{
+			printf("%ld, ", fib[i]);
+		}
+
+	}
+	return (0);
+}
