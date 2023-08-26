c265989aeaf462218474dfb802537d5704a662d5
malvinnthapeleng <malvinnthapeleng>
2023-08-26 04:30:38 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100.ret13.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rot13- encoder rot13
+ * @str: pointer to string str
+ * Return: *str
+ */
+char *rot13(char *str)
+{
+        int i, j;
+
+        for (i = 0; str[i] != '\0'; i++)
+        {
+                if (str[i] >= 'a' && str[i] <= 'z')
+                {
+                        str[i] = ((str[i] - 'a' + 13) % 26) + 'a';
+                }
+                else if (str[i] >= 'A' && str[i] <= 'Z')
+                {
+                        str[i] = ((str[i] - 'A' + 13) % 26) + 'A';
+                }
+        }
+        return (str);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,5 +10,6 @@ char *string_toupper(char *);
 char *cap_string(char *);
 int _putchar(char c);
 char *leet(char *);
+char *rot13(char *);
 
 #endif
