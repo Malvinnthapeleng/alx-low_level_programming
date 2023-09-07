e05f6e7405fb6d6bb282e8812179f7fd3f7d9510
malvinnthapeleng <malvinnthapeleng>
2023-09-07 02:45:02 -0700

    infinites

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,52 +1,79 @@
 #include "main.h"
+
 /**
-*infinite_add- function that adds two numbers
-*print_buffer- function that prints a buffer.
-*@n1: first number as input
-*@n2: second number as input
-*@r: buffer that the function will use to store the result
-*@size_r: buffer size
-*Return: 0 If the result can not be stored in r,else 1
-*/
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
+ */
+
+void rev_string(char *n)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
+}
+
+/**
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
+ */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int overflow = 0, i = 0, j = 0, digit = 0;
-int value1 = 0, value2 = 0, temp_total = 0;
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
-while (*(n1 + i) != '\0')
-i++;
-while (*(n2 + j) != '\0')
-j++;
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
+		return (0);
+	while (j >= 0 || i >= 0 || overflow == 1)
+	{
+		if (i < 0)
+			val1 = 0;
+		else
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
+	}
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
+	return (r);
+}
 
-i--;
-j--;
-if (i >= size_r || j >= size_r)
-return (0);
-while (i >= 0 || j >= 0 || overflow == 1)
-{
-if (i < 0)
-value1 = 0;
-else
-value1 = *(n1 + i) -48;
-if (j < 0)
-value2 = 0;
-else
-value2 *(n2 + j) - 48;
-temp_total = value1 + value2 + overflow;
-if (temp_total >= 10)
-overflow = 1;
-else
-overflow = 0;
-if (digit >= (size_r - 1))
-return (0);
-*(r + digit) = (temp_total % 10) + 48;
-digit++;
-j--;
-i--;
-}
-if (digit == size_r)
-return (0);
-*(r + digit) = '\0';
-rev_string(r);
-return (r);
-}
