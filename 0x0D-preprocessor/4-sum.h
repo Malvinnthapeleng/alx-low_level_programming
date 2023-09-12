13fa310a7deb0784943ddcbe338d6d8e5bddb2a1
malvinnthapeleng <malvinnthapeleng>
2023-09-11 19:10:52 -0700

    function-like macro SUM(x, y) that computes the sum of the numbers x and y

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,14 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * SUM - Calculates the sum of two values.
+ * @x: The first value.
+ * @y: The second value.
+ *
+ * Return: The sum of x and y.
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
