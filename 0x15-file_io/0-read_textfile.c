97953c3aa672af5e21379c3bf11b03c19724a6be
malvinnthapeleng <malvinnthapeleng@gmail.com>
2023-10-04 03:48:23 -0700

    a function that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,52 +1,54 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+
 /**
 * read_textfile - Read text file and print to STDOUT.
-* @filename: Name of the text file to read.
+* @filename: Text file being read.
 * @letters: Number of letters to be read.
-* Return: Number of bytes read and printed on success, 0 on failure.
+*
+* Return: Actual number of bytes read and printed, 0 when function fails,
+* or filename is NULL.
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd;        /* File descriptor */
-ssize_t bytes; /* Number of bytes read and printed */
-char *buf;     /* Buffer to store read data */
+char *buf;
+ssize_t fd;
+ssize_t w;
+ssize_t t;
 
 if (!filename)
 return (0);
 
 fd = open(filename, O_RDONLY);
 if (fd == -1)
-{
-perror("Error opening file");
 return (0);
-}
 
 buf = malloc(sizeof(char) * letters);
 if (!buf)
 {
-close(fd); /* Close the file before returning */
-perror("Memory allocation error");
+close(fd);
 return (0);
 }
 
-bytes = read(fd, buf, letters);
-if (bytes == -1)
+t = read(fd, buf, letters);
+if (t == -1)
 {
-perror("Error reading file");
-free(buf);  /* Free allocated memory before returning */
-close(fd);  /* Close the file before returning */
-return (0);
-}
-if (write(STDOUT_FILENO, buf, bytes) == -1)
-{
-perror("Error writing to STDOUT");
-free(buf);  /* Free allocated memory before returning */
-close(fd);  /* Close the file before returning */
-return (0);
-}
 free(buf);
 close(fd);
-return (bytes);
+return (0);
 }
+
+w = write(STDOUT_FILENO, buf, t);
+if (w == -1)
+{
+free(buf);
+close(fd);
+return (0);
+}
+
+free(buf);
+close(fd);
+return (w);
+}
+
